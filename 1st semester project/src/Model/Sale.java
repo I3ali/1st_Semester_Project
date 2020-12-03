ef5e16d2652730546624be3b4394ab2214ddbaaf
I3ali <71490447+I3ali@users.noreply.github.com>
2020-12-03 12:07:21 +0100

    sale

--- 1st semester project/src/Model/Sale.java
+++ 1st semester project/src/Model/Sale.java
@@ -9,6 +9,62 @@ public class Sale {
     private int price;
     private LocalDateTime dueTime;
     private String department;
-    private String haha;
 
+    public Sale (int ID, String costumer, int price, LocalDateTime dueTime, String department) {
+
+        this.costumer = costumer;
+        this.price = price;
+        this.dueTime = dueTime;
+        this.department = department;
+        listOfProducts = new ArrayList<>();
+
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getCostumer() {
+        return costumer;
+    }
+
+    public void setCostumer(String costumer) {
+        this.costumer = costumer;
+    }
+
+    public ArrayList<Integer> getListOfProducts() {
+        return listOfProducts;
+    }
+
+    public void setListOfProducts(ArrayList<Integer> listOfProducts) {
+        this.listOfProducts = listOfProducts;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public LocalDateTime getDueTime() {
+        return dueTime;
+    }
+
+    public void setDueTime(LocalDateTime dueTime) {
+        this.dueTime = dueTime;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
 }
