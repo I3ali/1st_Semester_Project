ab7f1742badeabe3907ed2b404ad39dbd5050226
Marton Vadasz <75419452+VADMARCI@users.noreply.github.com>
2020-12-03 10:54:06 +0100

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,57 @@
+package Model;
+
+public class Customer {
+    private String name;
+    private int ID;
+    private String group;
+    private String email;
+    private int phone;
+    public Customer (String name, int ID, String group, String email,
+                     int phone) {
+        this.name = name;
+        this.ID = ID;
+        this.group = group;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+}
---
+++ Sale.java
@@ -0,0 +1,15 @@
+package Model;
+import java.util.*;
+import java.time.LocalDateTime;
+
+public class Sale {
+    private int ID;
+    private String costumer;
+    private ArrayList<Integer> listOfProducts;
+    private int price;
+    private LocalDateTime dueTime;
+    private String department;
+    private 
+
+    private
+}
